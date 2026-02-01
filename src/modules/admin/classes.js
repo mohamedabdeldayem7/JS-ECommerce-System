60d2474813231c956cf87fbdedb1118c2f3d07e2
mohamedabdeldayem7 <mohamed.bk10150@gmail.com>
2026-02-01 05:33:14 +0200

    implement Product class and Category class

---
+++ src/modules/admin/classes.js
@@ -0,0 +1,111 @@
+export class Product {
+  constructor(
+    id = Date.now(),
+    name,
+    description,
+    categoryId,
+    image,
+    price,
+    stockQuantity,
+  ) {
+    // validation need in each field
+    this.id = id;
+    this.setName = name;
+    this.setCategoryId = categoryId;
+    this.setDescription = description;
+    this.setImage = image;
+    this.setPrice = price;
+    this.setStockQuantity = stockQuantity;
+  }
+
+  // ID
+  get getId() {
+    return this.id;
+  }
+
+  // Name
+  get getName() {
+    return this.name;
+  }
+  set setName(value) {
+    if (typeof value !== "string" || value.trim() === "")
+      throw new Error("Invalid name.");
+    this.name = value;
+  }
+
+  // Description
+  get getDescription() {
+    return this.description;
+  }
+  set setDescription(value) {
+    this.description = value || "No description provided.";
+  }
+
+  // Category ID
+  get gerCategoryId() {
+    return this.categoryId;
+  }
+  set setCategoryId(value) {
+    if (!value) throw new Error("Category ID is required.");
+    this.categoryId = value;
+  }
+
+  // Image
+  get getImage() {
+    return this.image;
+  }
+  set setImage(value) {
+    this.image = value || "default-placeholder.png";
+  }
+
+  // Price
+  get getPrice() {
+    return this.price;
+  }
+  set setPrice(value) {
+    if (typeof value !== "number" || value < 0)
+      throw new Error("Price must be a positive number.");
+    this.price = value;
+  }
+
+  // Stock Quantity
+  get getStockQuantity() {
+    return this.stockQuantity;
+  }
+  set setStockQuantity(value) {
+    if (!Number.isInteger(value) || value < 0)
+      throw new Error("Stock must be a non-negative integer.");
+    this.stockQuantity = value;
+  }
+}
+
+export class Category {
+  constructor(id = Date.now(), name, description) {
+    this.id = id;
+    this.setName = name;
+    this.setDescription = description;
+  }
+
+  // ID
+  get getId() {
+    return this.id;
+  }
+
+  // Name
+  get getName() {
+    return this.name;
+  }
+  set setName(value) {
+    if (typeof value !== "string" || value.trim() === "")
+      throw new Error("Invalid name.");
+    this.name = value;
+  }
+
+  // Description
+  get getDescription() {
+    return this.description;
+  }
+  set setDescription(value) {
+    this.description = value || "No description provided.";
+  }
+}
