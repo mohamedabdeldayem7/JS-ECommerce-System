47cbf2ae3c8a3f38ff9109ab8227fe6d16849027
mohamedabdeldayem7 <mohamed.bk10150@gmail.com>
2026-02-03 00:06:44 +0200

    edit categories IDs in product list

--- src/utils/storage/dummyData.js
+++ src/utils/storage/dummyData.js
@@ -147,7 +147,7 @@ const fakeProducts = [
     price: 56.99,
     description:
       "Note:The Jackets is US standard size, Please choose size as your usual wear Material: 100% Polyester; Detachable Liner Fabric: Warm Fleece. Detachable Functional Liner: Skin Friendly, Lightweigt and Warm.Stand Collar Liner jacket, keep you warm in cold weather. Zippered Pockets: 2 Zippered Hand Pockets, 2 Zippered Pockets on Chest (enough to keep cards or keys)and 1 Hidden Pocket Inside.Zippered Hand Pockets and Hidden Pocket keep your things secure. Humanized Design: Adjustable and Detachable Hood and Adjustable cuff to prevent the wind and water,for a comfortable fit. 3 in 1 Detachable Design provide more convenience, you can separate the coat and inner as needed, or wear it together. It is suitable for different season and help you adapt to different climates",
-    categoryId: 4,
+    categoryId: 2,
     image: "https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_t.png",
     stockQuantity: 235,
   },
@@ -157,7 +157,7 @@ const fakeProducts = [
     price: 29.95,
     description:
       "100% POLYURETHANE(shell) 100% POLYESTER(lining) 75% POLYESTER 25% COTTON (SWEATER), Faux leather material for style and comfort / 2 pockets of front, 2-For-One Hooded denim style faux leather jacket, Button detail on waist / Detail stitching at sides, HAND WASH ONLY / DO NOT BLEACH / LINE DRY / DO NOT IRON",
-    categoryId: 4,
+    categoryId: 2,
     image: "https://fakestoreapi.com/img/81XH0e8fefL._AC_UY879_t.png",
     stockQuantity: 340,
   },
@@ -167,7 +167,7 @@ const fakeProducts = [
     price: 39.99,
     description:
       "Lightweight perfet for trip or casual wear---Long sleeve with hooded, adjustable drawstring waist design. Button and zipper front closure raincoat, fully stripes Lined and The Raincoat has 2 side pockets are a good size to hold all kinds of things, it covers the hips, and the hood is generous but doesn't overdo it.Attached Cotton Lined Hood with Adjustable Drawstrings give it a real styled look.",
-    categoryId: 4,
+    categoryId: 2,
     image: "https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2t.png",
     stockQuantity: 679,
   },
@@ -177,7 +177,7 @@ const fakeProducts = [
     price: 9.85,
     description:
       "95% RAYON 5% SPANDEX, Made in USA or Imported, Do Not Bleach, Lightweight fabric with great stretch for comfort, Ribbed on sleeves and neckline / Double stitching on bottom hem",
-    categoryId: 4,
+    categoryId: 2,
     image: "https://fakestoreapi.com/img/71z3kpMAYsL._AC_UY879_t.png",
     stockQuantity: 130,
   },
@@ -187,7 +187,7 @@ const fakeProducts = [
     price: 7.95,
     description:
       "100% Polyester, Machine wash, 100% cationic polyester interlock, Machine Wash & Pre Shrunk for a Great Fit, Lightweight, roomy and highly breathable with moisture wicking fabric which helps to keep moisture away, Soft Lightweight Fabric with comfortable V-neck collar and a slimmer fit, delivers a sleek, more feminine silhouette and Added Comfort",
-    categoryId: 4,
+    categoryId: 2,
     image: "https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_t.png",
     stockQuantity: 146,
   },
@@ -197,7 +197,7 @@ const fakeProducts = [
     price: 12.99,
     description:
       "95%Cotton,5%Spandex, Features: Casual, Short Sleeve, Letter Print,V-Neck,Fashion Tees, The fabric is soft and has some stretch., Occasion: Casual/Office/Beach/School/Home/Street. Season: Spring,Summer,Autumn,Winter.",
-    categoryId: 4,
+    categoryId: 2,
     image: "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_t.png",
     stockQuantity: 145,
   },
@@ -245,7 +245,7 @@ const category4 = new Category(
 const categories = [category1, category2, category3, category4];
 
 // products
-const products = fakeProducts.map(
+export const products = fakeProducts.map(
   (p) =>
     new Product(
       p.id,
@@ -258,11 +258,11 @@ const products = fakeProducts.map(
     ),
 );
 
-console.log(products);
+// console.log(products);
 
-// create StorageManager object
-localStorage.clear();
-const storageManager = new StorageManager();
+// // create StorageManager object
+// localStorage.clear();
+// const storageManager = new StorageManager();
 
-storageManager.set("products", products);
-storageManager.set("categories", categories);
+// storageManager.set("products", products);
+// storageManager.set("categories", categories);
