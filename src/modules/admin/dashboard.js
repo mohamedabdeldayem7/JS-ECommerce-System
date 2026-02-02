6117eca0557fcbe07f273e2c8b57e7d71698c4f7
mohamedabdeldayem7 <mohamed.bk10150@gmail.com>
2026-02-03 00:05:49 +0200

    added js code to setup product modal for addition and edit

--- src/modules/admin/dashboard.js
+++ src/modules/admin/dashboard.js
@@ -1,3 +1,6 @@
+// sidebar
+
+// activate background color with low opacity under sidebar
 function toggleSidebar() {
   const sidebar = document.getElementById("sidebar");
   const overlay = document.getElementById("overlay");
@@ -8,25 +11,73 @@ function toggleSidebar() {
 
 document.querySelectorAll(".sidebar .nav-link[data-target]").forEach((link) => {
   link.addEventListener("click", function (e) {
-    // 1. إزالة الحالة النشطة من كل الروابط
     document
       .querySelectorAll(".sidebar .nav-link")
       .forEach((l) => l.classList.remove("active"));
-    // 2. إضافة الحالة النشطة للرابط الحالي
     this.classList.add("active");
+    document.getElementById("main-dashboard").classList.add("active");
 
-    // 3. إخفاء كل السكاشن
     document
       .querySelectorAll(".content-section")
       .forEach((section) => section.classList.add("d-none"));
 
-    // 4. إظهار السكشن المطلوب
     const targetId = this.getAttribute("data-target");
     document.getElementById(targetId).classList.remove("d-none");
 
-    // 5. إغلاق السايدبار إذا كنا في وضع الموبايل
     if (window.innerWidth < 768) {
       toggleSidebar();
     }
   });
 });
+
+// open product modal
+
+let products = [
+  {
+    id: "P-8821",
+    name: "Wireless Headphones Ultra",
+    category: "Electronics",
+    price: 249.0,
+    stock: 124,
+    status: "Active",
+  },
+  {
+    id: "P-8822",
+    name: "Full Grain Leather Wallet",
+    category: "Accessories",
+    price: 85.0,
+    stock: 8,
+    status: "Low Stock",
+  },
+];
+const productModal = new bootstrap.Modal(
+  document.getElementById("productModal"),
+);
+const productForm = document.getElementById("productForm");
+
+// on add product btn
+function openAddModal() {
+  document.getElementById("modalTitle").innerText = "Add New Product";
+  document.getElementById("saveBtn").innerText = "Save Product";
+  document.getElementById("editProductId").value = "";
+  productForm.reset();
+  productModal.show();
+}
+
+// on edit product btn
+function openEditModal(id) {
+  const product = products.find((p) => p.id === id);
+
+  if (product) {
+    document.getElementById("modalTitle").innerText = "Edit Product";
+    document.getElementById("saveBtn").innerText = "Update Changes";
+
+    document.getElementById("editProductId").value = product.id;
+    document.getElementById("prodName").value = product.name;
+    document.getElementById("prodCategory").value = product.category;
+    document.getElementById("prodPrice").value = product.price;
+    document.getElementById("prodStock").value = product.stock;
+
+    productModal.show();
+  }
+}
